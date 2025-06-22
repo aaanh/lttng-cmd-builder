0d327d7446e6ee9bff32969f8ee368cfcf09ec28
Anh Hoang Nguyen <AnhNguyen@aaanh.com>
2025-06-21 23:11:02 -0400

    Improve styling, fix event filtering

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,6 @@ export default defineConfig({
   build: {
     inlineStylesheets: "auto",
   },
-  // Uncomment the following line if your site is deployed to a subdirectory
-  // base: '/lttng-cmd-builder',
+  site: "https://lttng-cmd-builder.vercel.app",
+  // base: "lttng-cmd-builder",
 });
