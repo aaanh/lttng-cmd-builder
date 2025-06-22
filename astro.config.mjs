13846770dd7a4018bb428f07285aa2f319d6c3fc
Anh Hoang Nguyen <AnhNguyen@aaanh.com>
2025-06-21 22:44:59 -0400

    Fix stylesheet missing

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,9 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  build: {
+    inlineStylesheets: "auto",
+  },
+  // Uncomment the following line if your site is deployed to a subdirectory
+  // base: '/lttng-cmd-builder',
 });
